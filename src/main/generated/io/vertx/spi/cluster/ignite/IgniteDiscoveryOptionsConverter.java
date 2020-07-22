ee7387f8204a6610233564081d88b583ba16da84
Lukas Prettenthaler <lukas@noenv.com>
2020-07-22 12:02:46 +0200

    add generated files

---
+++ src/main/generated/io/vertx/spi/cluster/ignite/IgniteDiscoveryOptionsConverter.java
@@ -0,0 +1,44 @@
+package io.vertx.spi.cluster.ignite;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Converter and mapper for {@link io.vertx.spi.cluster.ignite.IgniteDiscoveryOptions}.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.spi.cluster.ignite.IgniteDiscoveryOptions} original class using Vert.x codegen.
+ */
+public class IgniteDiscoveryOptionsConverter {
+
+
+  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, IgniteDiscoveryOptions obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "properties":
+          if (member.getValue() instanceof JsonObject) {
+            obj.setProperties(((JsonObject)member.getValue()).copy());
+          }
+          break;
+        case "type":
+          if (member.getValue() instanceof String) {
+            obj.setType((String)member.getValue());
+          }
+          break;
+      }
+    }
+  }
+
+  public static void toJson(IgniteDiscoveryOptions obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+  public static void toJson(IgniteDiscoveryOptions obj, java.util.Map<String, Object> json) {
+    if (obj.getProperties() != null) {
+      json.put("properties", obj.getProperties());
+    }
+    if (obj.getType() != null) {
+      json.put("type", obj.getType());
+    }
+  }
+}
---
+++ src/main/generated/io/vertx/spi/cluster/ignite/IgniteSslOptionsConverter.java
@@ -0,0 +1,98 @@
+package io.vertx.spi.cluster.ignite;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Converter and mapper for {@link io.vertx.spi.cluster.ignite.IgniteSslOptions}.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.spi.cluster.ignite.IgniteSslOptions} original class using Vert.x codegen.
+ */
+public class IgniteSslOptionsConverter {
+
+
+  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, IgniteSslOptions obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "keyAlgorithm":
+          if (member.getValue() instanceof String) {
+            obj.setKeyAlgorithm((String)member.getValue());
+          }
+          break;
+        case "keyStoreFilePath":
+          if (member.getValue() instanceof String) {
+            obj.setKeyStoreFilePath((String)member.getValue());
+          }
+          break;
+        case "keyStorePassword":
+          if (member.getValue() instanceof String) {
+            obj.setKeyStorePassword((String)member.getValue());
+          }
+          break;
+        case "keyStoreType":
+          if (member.getValue() instanceof String) {
+            obj.setKeyStoreType((String)member.getValue());
+          }
+          break;
+        case "protocol":
+          if (member.getValue() instanceof String) {
+            obj.setProtocol((String)member.getValue());
+          }
+          break;
+        case "trustAll":
+          if (member.getValue() instanceof Boolean) {
+            obj.setTrustAll((Boolean)member.getValue());
+          }
+          break;
+        case "trustStoreFilePath":
+          if (member.getValue() instanceof String) {
+            obj.setTrustStoreFilePath((String)member.getValue());
+          }
+          break;
+        case "trustStorePassword":
+          if (member.getValue() instanceof String) {
+            obj.setTrustStorePassword((String)member.getValue());
+          }
+          break;
+        case "trustStoreType":
+          if (member.getValue() instanceof String) {
+            obj.setTrustStoreType((String)member.getValue());
+          }
+          break;
+      }
+    }
+  }
+
+  public static void toJson(IgniteSslOptions obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+  public static void toJson(IgniteSslOptions obj, java.util.Map<String, Object> json) {
+    if (obj.getKeyAlgorithm() != null) {
+      json.put("keyAlgorithm", obj.getKeyAlgorithm());
+    }
+    if (obj.getKeyStoreFilePath() != null) {
+      json.put("keyStoreFilePath", obj.getKeyStoreFilePath());
+    }
+    if (obj.getKeyStorePassword() != null) {
+      json.put("keyStorePassword", obj.getKeyStorePassword());
+    }
+    if (obj.getKeyStoreType() != null) {
+      json.put("keyStoreType", obj.getKeyStoreType());
+    }
+    if (obj.getProtocol() != null) {
+      json.put("protocol", obj.getProtocol());
+    }
+    json.put("trustAll", obj.isTrustAll());
+    if (obj.getTrustStoreFilePath() != null) {
+      json.put("trustStoreFilePath", obj.getTrustStoreFilePath());
+    }
+    if (obj.getTrustStorePassword() != null) {
+      json.put("trustStorePassword", obj.getTrustStorePassword());
+    }
+    if (obj.getTrustStoreType() != null) {
+      json.put("trustStoreType", obj.getTrustStoreType());
+    }
+  }
+}
