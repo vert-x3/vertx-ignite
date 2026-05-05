c2da9db7b973268f61d13af6c10792f9fd740e1e
Lukas Prettenthaler <rdx@wsn.at>
2026-05-05 15:49:13 +0200

    Add same parameters to IgniteFaultToleranceTest like in any other test suite

--- src/test/java/io/vertx/core/eventbus/IgniteFaultToleranceTest.java
+++ src/test/java/io/vertx/core/eventbus/IgniteFaultToleranceTest.java
@@ -19,6 +19,8 @@ import io.vertx.Lifecycle;
 import io.vertx.core.Vertx;
 import io.vertx.core.spi.cluster.ClusterManager;
 import io.vertx.spi.cluster.ignite.IgniteClusterManager;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,9 +32,26 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
  */
 public class IgniteFaultToleranceTest extends io.vertx.tests.eventbus.FaultToleranceTest {
 
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+  private String igniteHome;
+
   @Override
   protected ClusterManager getClusterManager() {
     return new IgniteClusterManager();
+  }
+
+  @Override
+  public void setUp() throws Exception {
+    igniteHome = temporaryFolder.newFolder().getAbsolutePath();
+    super.setUp();
+  }
+
+  @Override
+  public void tearDown() throws Exception {
+    super.tearDown();
+    igniteHome = null;
   }
 
   @Override
@@ -43,6 +62,12 @@ public class IgniteFaultToleranceTest extends io.vertx.tests.eventbus.FaultToler
       return Arrays.asList(
         "-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory",
         "-Djava.net.preferIPv4Stack=true",
+        "-DIGNITE_HOME=" + igniteHome,
+        "-DIGNITE_PERFORMANCE_SUGGESTIONS_DISABLED=true",
+        "-DIGNITE_NO_ASCII=true",
+        "-DIGNITE_UPDATE_NOTIFIER=false",
+        "-DIGNITE_ATOMIC_CACHE_DELETE_HISTORY_SIZE=1000",
+        "-DIGNITE_AFFINITY_HISTORY_SIZE=500",
         "--add-opens=java.base/java.nio=ALL-UNNAMED",
         "--add-opens=java.base/java.util=ALL-UNNAMED",
         "--add-opens=java.base/java.lang.invoke=ALL-UNNAMED",
@@ -51,7 +76,13 @@ public class IgniteFaultToleranceTest extends io.vertx.tests.eventbus.FaultToler
     } catch(ClassNotFoundException e) {
       return Arrays.asList(
         "-Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory",
-        "-Djava.net.preferIPv4Stack=true"
+        "-Djava.net.preferIPv4Stack=true",
+        "-DIGNITE_HOME=" + igniteHome,
+        "-DIGNITE_PERFORMANCE_SUGGESTIONS_DISABLED=true",
+        "-DIGNITE_NO_ASCII=true",
+        "-DIGNITE_UPDATE_NOTIFIER=false",
+        "-DIGNITE_ATOMIC_CACHE_DELETE_HISTORY_SIZE=1000",
+        "-DIGNITE_AFFINITY_HISTORY_SIZE=500"
       );
     }
   }
